17ab4ebe142feab7648ed9c5954270277a1e6dab
Fantalover <sunkistsugar2@gmail.com>
2026-07-20 14:33:54 +1000

    Update Loader UI.lua

--- Game/Loader UI.lua
+++ Game/Loader UI.lua
@@ -1,23 +1,34 @@
+--[[
+██╗  ██╗ ██████╗ ██████╗ ██╗███████╗ ██████╗ ███╗   ██╗
+██║  ██║██╔═══██╗██╔══██╗██║╚══███╔╝██╔═══██╗████╗  ██║
+███████║██║   ██║██████╔╝██║  ███╔╝ ██║   ██║██╔██╗ ██║
+██╔══██║██║   ██║██╔══██╗██║ ███╔╝  ██║   ██║██║╚██╗██║
+██║  ██║╚██████╔╝██║  ██║██║███████╗╚██████╔╝██║ ╚████║
+╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚═╝╚══════╝ ╚═════╝ ╚═╝  ╚═══╝
+
+                    Horizon
+]]
+
+
 local Loader = loadstring(game:HttpGet("https://vanta.my/loader-ui"))()
 
 Loader:Create({
-Name = "Horizon",
-Status = "Working / Undetected",
-LastUpdated = "20/7/2026",
-Version = "V1.0.0",
-Type = "Free",
+    Name = "Horizon",
+    Status = "Working / Undetected",
+    LastUpdated = "20/7/2026",
+    Version = "V1.0.0",
+    Type = "Free",
 
--- Optional URL to fetch these statuses live (returns JSON)  
--- ConfigURL = "https://pastebin.com/raw/...",  
-  
-EnableTOS = false,  
-TOSLink = "",  
-  
-Scripts = {  
-    "Loader (PC)"
+    EnableTOS = false,
+    TOSLink = "",
 
-},
-URLs = {
-["Loader (PC)"] = "https://github.com/fantaloverzaa/Horizon/blob/main/pc.lua"
-}
-}
+    Scripts = {
+        "Loader (PC)",
+        "Soon",
+        "Soon"
+    },
+
+    URLs = {
+        ["Loader (PC)"] = "https://raw.githubusercontent.com/fantaloverzaa/Horizon/main/pc.lua"
+    }
+})
