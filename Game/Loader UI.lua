ac420451fc806edf5069ab3e660794bbeb959ec5
Fantalover <sunkistsugar2@gmail.com>
2026-07-20 13:09:45 +1000

    Create Loader UI.lua

---
+++ Game/Loader UI.lua
@@ -0,0 +1,23 @@
+local Loader = loadstring(game:HttpGet("https://vanta.my/loader-ui"))()
+
+Loader:Create({
+Name = "Horizon",
+Status = "Working / Undetected",
+LastUpdated = "20/7/2026",
+Version = "V1.0.0",
+Type = "Free",
+
+-- Optional URL to fetch these statuses live (returns JSON)  
+-- ConfigURL = "https://pastebin.com/raw/...",  
+  
+EnableTOS = false,  
+TOSLink = "",  
+  
+Scripts = {  
+    "Loader (PC)"
+
+},
+URLs = {
+["Loader (PC)"] = "https://horizonxyz.netlify.app/scripts/loader.lua"
+}
+}
